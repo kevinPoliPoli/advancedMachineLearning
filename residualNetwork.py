b7a3fce5bc54b37bfb4cf813ad480bdaa467c21d
KEVIN CARDINALE S305035 <s305035@studenti.polito.it>
2022-11-04 10:06:03 +0100

    fixed train.py and evaluate.py

--- residualNetwork.py
+++ residualNetwork.py
@@ -1,3 +1,5 @@
+import torch.nn as nn
+
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
         super(ResidualBlock, self).__init__()
--- train.py
+++ train.py
@@ -1,59 +1,57 @@
-class Train():    
+def train(epoch, net):
 
-    def train(epoch, net):
+    net = net.to(DEVICE)
 
-        net = net.to(DEVICE)
+    net.train(True) # Sets module in training mode
 
-        net.train(True) # Sets module in training mode
+    running_loss=0
+    correct=0
+    total=0
 
-        running_loss=0
-        correct=0
-        total=0
+    current_step = 0 # ?????
+    
+    # Iterate over the dataset
+    for images, labels in train_dataloader:
+        # Bring data over the device of choice
+        images = images.to(DEVICE)
+        labels = labels.to(DEVICE)
 
-        current_step = 0 # ?????
-        
-        # Iterate over the dataset
-        for images, labels in train_dataloader:
-            # Bring data over the device of choice
-            images = images.to(DEVICE)
-            labels = labels.to(DEVICE)
+        # PyTorch, by default, accumulates gradients after each backward pass
+        # We need to manually set the gradients to zero before starting a new iteration
+        optimizer.zero_grad() # Zero-ing the gradients
 
-            # PyTorch, by default, accumulates gradients after each backward pass
-            # We need to manually set the gradients to zero before starting a new iteration
-            optimizer.zero_grad() # Zero-ing the gradients
+        # Forward pass to the network
+        outputs = net(images)
 
-            # Forward pass to the network
-            outputs = net(images)
+        # Compute loss based on output and ground truth
+        loss = criterion(outputs, labels)
 
-            # Compute loss based on output and ground truth
-            loss = criterion(outputs, labels)
+        """
+        # Log loss
+        if current_step % LOG_FREQUENCY == 0:
+        print('Step {}, Loss {}'.format(current_step, loss.item()))
+        """
 
-            """
-            # Log loss
-            if current_step % LOG_FREQUENCY == 0:
-            print('Step {}, Loss {}'.format(current_step, loss.item()))
-            """
+        # Compute gradients for each layer and update weights
+        loss.backward()  # backward pass: computes gradients
+        optimizer.step() # update weights based on accumulated gradients
 
-            # Compute gradients for each layer and update weights
-            loss.backward()  # backward pass: computes gradients
-            optimizer.step() # update weights based on accumulated gradients
+        running_loss += loss.item()
 
-            running_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
 
-            _, predicted = outputs.max(1)
-            total += labels.size(0)
-            correct += predicted.eq(labels).sum().item()
+        current_step += 1
 
-            current_step += 1
+    train_loss=running_loss/len(train_dataloader)
+    accu=100.*correct/total
 
-        train_loss=running_loss/len(train_dataloader)
-        accu=100.*correct/total
+    train_accu.append(accu)
+    train_losses.append(train_loss)
+    print('Train Loss: %.3f | Train Accuracy: %.3f'%(train_loss,accu))
 
-        train_accu.append(accu)
-        train_losses.append(train_loss)
-        print('Train Loss: %.3f | Train Accuracy: %.3f'%(train_loss,accu))
+    # Step the scheduler
+    scheduler.step() 
 
-        # Step the scheduler
-        scheduler.step() 
-
-        return net
+    return net
